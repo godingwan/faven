0f073d89a29e268c8f9ad0968dea3aa40997fa70
Joshua Lee <jsungholee@gmail.com>
2013-06-17 13:17:32 -0400

    create user sign in page
    [#51773007]

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,11 +1,36 @@
 require 'spec_helper'
 
 feature User do
+  let(:user) do
+    @previous_count = User.count
+    @user = User.new
+  end
+
   scenario 'user account is not created unless all info is presented' do
-    user = User.new
+    user
 
-    user.save
+    @user.save
 
-    expect(user.valid?).to eql(false)
+    expect(User.count).to eql(@previous_count)
+  end
+
+  scenario 'user account is not created if email is not valid' do
+    user
+
+    @user.email = 'notvalidemail'
+    @user.password = 'abc123456'
+    @user.save
+
+    expect(User.count).to eql(@previous_count)
+  end
+
+  scenario 'user account is not created if password is not long enough' do
+    user
+
+    @user.email = "email@mail.com"
+    @user.password = "short"
+    @user.save
+
+    expect(User.count).to eql(@previous_count)
   end
 end
