0bbd7ec6503213b853a3f96877b8ffabf25fb830
Joshua Lee <jsungholee@gmail.com>
2013-06-18 12:39:53 -0400

    user sign out
    Created user sign out link that is placed on all pages. It will only show if the user is signed in.
    
    [#51814479]

---
+++ spec/features/user_sign_out_spec.rb
@@ -0,0 +1,19 @@
+require 'spec_helper'
+
+feature 'user signing out' do
+  let(:user) { FactoryGirl.create(:user) }
+
+  scenario 'user must see a sign out link' do
+    sign_in_as(user)
+
+    expect(page).to have_content("Sign out")
+  end
+
+  scenario 'must not see sign out if already signed out' do
+    sign_in_as(user)
+
+    click_link "Sign out"
+
+    expect(page).to_not have_content("Sign out")
+  end
+end
