743f78898edcc874c78c03a09c8ebe122044bd91
Joshua Lee <jsungholee@gmail.com>
2013-07-10 11:15:15 -0400

    draw on canvas but hidden

---
+++ app/assets/javascripts/views/konami.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+  var keys = [];
+  var konami = '38,38,40,40,37,39,37,39,66,65';
+
+  $(document).keydown(
+    function(e){
+      keys.push( e.keyCode );
+      if ( keys.toString().indexOf( konami ) >= 0 ){
+        $('#canvas_div').show();
+        keys = [];
+      }
+    })
+});
