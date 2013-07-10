603144aef49bfb50d341cdc37af3a852fdd43b79
Joshua Lee <jsungholee@gmail.com>
2013-07-10 11:15:32 -0400

    draw on canvas and hidden real one

--- app/assets/javascripts/views/canvas.js
+++ app/assets/javascripts/views/canvas.js
@@ -1,28 +1,52 @@
 var c = document.querySelector('canvas');
 var cx = c.getContext('2d');
 var mousedown = false;
+var oldx = null;
+var oldy = null;
 
 function setupCanvas() {
   c.height = 480;
-  c.width = 320;
-  cx.lineWidth = 20;
+  c.width = 960;
+  cx.lineWidth = 10;
   cx.lineCap = 'round';
   cx.strokeStyle = 'rgb(0, 0, 50)';
 }
-
+function onmousedown(ev) {
+  mousedown = true;
+  oldx = ev.clientX;
+  oldy = ev.clientY;
+  ev.preventDefault();
+}
+function onmouseup(ev) {
+  mousedown = false;
+  ev.preventDefault();
+}
 function onmousemove(ev) {
   var x = ev.clientX;
   var y = ev.clientY;
-  paint(x, y);
+  if (mousedown) {
+    paint(x, y);
+  }
 }
-
 function paint(x, y) {
-  cx.beginPath();
-  cx.moveTo(x, y);
+  if (oldx > 0 && oldy > 0) {
+    cx.moveTo(oldx, oldy);
+  }
   cx.lineTo(x, y);
   cx.stroke();
-  cx.closePath();
+  oldx = x;
+  oldy = y;
 }
+
+c.addEventListener('mousedown', onmousedown, false);
+c.addEventListener('mouseup', onmouseup, false);
 c.addEventListener('mousemove', onmousemove, false);
 
 setupCanvas();
+
+// jquery version
+$('#canvas_div').hide();
+
+// javascript version of top
+// var canvas = document.getElementById('canvas_div');
+// canvas.style.display = 'none';
